9cd43f130cecb2d4b45a632406396f8a057316eb
varkolu1729 <varkolu.vijay1729@gmail.com>
2024-01-27 15:32:10 +0530

    again

--- pp.py
+++ pp.py
@@ -1,2 +1,10 @@
 class v:
-    pass
+    class_variable = 0
+    def __init__(self):
+        self.instance_variable = 0
+    
+
+if __name__ == "__main__":
+    obj =  v()
+    print(obj.instance_variable)
+    print(obj.class_variable)
