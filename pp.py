a443e41a967e8f21ddf533b21f77eee91c615756
varkolu1729 <varkolu.vijay1729@gmail.com>
2024-02-01 01:29:02 +0530

    new notebook added

--- pp.py
+++ pp.py
@@ -1,10 +1,25 @@
-class v:
-    class_variable = 0
-    def __init__(self):
-        self.instance_variable = 0
+# class v:
+#     class_variable = 0
+#     def __init__(self):
+#         self.instance_variable = 0
     
 
+# if __name__ == "__main__":
+#     obj =  v()
+#     print(obj.instance_variable)
+#     print(obj.class_variable)
+
+
+
+
+import flask as f
+
+app = f.Flask(__name__)
+
+@app.route("/")
+def vij():
+    dic = {1:"vijay"}
+    return f.render_template("index.html",word=str(dic))
+
 if __name__ == "__main__":
-    obj =  v()
-    print(obj.instance_variable)
-    print(obj.class_variable)
+    app.run()
